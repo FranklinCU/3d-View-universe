1fcda269210362072310df2d8ab18945be76f6d8
Franklin Cumplido <125756714+FranklinCU@users.noreply.github.com>
2025-11-10 11:53:47 -0500

    Add files via upload

---
+++ intentando_sistema_solar/visualization/__init__.py
@@ -0,0 +1,9 @@
+# visualization/__init__.py
+"""
+Módulo de visualización para generación procedural
+"""
+
+from .sphere_generator import ProceduralSphere
+from .shader_math import ShaderMath
+
+__all__ = ['ProceduralSphere', 'ShaderMath']
