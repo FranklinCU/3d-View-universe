043ff5b9d00500f11c77b957c0c29f612cd36a8e
Franklin Cumplido <125756714+FranklinCU@users.noreply.github.com>
2025-11-10 11:54:31 -0500

    Delete intentando_sistema_solar directory

--- intentando_sistema_solar/visualization/__init__.py
+++
@@ -1,9 +0,0 @@
-# visualization/__init__.py
-"""
-Módulo de visualización para generación procedural
-"""
-
-from .sphere_generator import ProceduralSphere
-from .shader_math import ShaderMath
-
-__all__ = ['ProceduralSphere', 'ShaderMath']
